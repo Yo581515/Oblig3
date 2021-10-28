587340107fa2be05c24346ddf393ea7395b4e259
581515 <47955690+Y581515@users.noreply.github.com>
2021-10-28 02:21:29 +0200

    i do not know how but it works

--- Oppgave1/src/main/java/oppgave1Packe/LoggInnServlet.java
+++ Oppgave1/src/main/java/oppgave1Packe/LoggInnServlet.java
@@ -12,7 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/" + LOGIN_URL)
+//@WebServlet("/" + LOGIN_URL)
+@WebServlet(name = "LoggInnServlet", urlPatterns = "/" + LOGIN_URL)
 public class LoggInnServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static String rettPassord = "Hemmeligt";
